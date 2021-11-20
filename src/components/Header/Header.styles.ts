02e04340c32c5440d7af823d7301d8cf46ed9fab
viniciospereirarodrigues@gmail.com <viniciospereirarodrigues@gmail.com>
2021-11-19 21:08:30 -0300

    Create and styling header

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -8,8 +8,27 @@ export const Container = styled.header`
 `;
 
 export const Wrapper = styled.div`
-  max-width: 1020px;
+  max-width: 67.5rem;
   margin: 0 auto;
+  padding-top: 1.5rem;
   display: flex;
   justify-content: space-between;
+`;
+
+export const Button = styled.button`
+  background: ${Colors.bgButton};
+  width: 10rem;
+  height: 3rem;
+  font-family: "Poppins";
+  font-weight: 600;
+  color: ${Colors.white};
+  border-radius: 0.25rem;
+  border: solid 1px ${Colors.bgButton};
+  cursor: pointer;
+  transition: 0.4s;
+
+
+  :hover{
+    filter: brightness(0.7);
+  }
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import * as S from "./Header.styles";
+import logoImg from '../../assets/logo.svg'
 export const Header = () => {
   return (
     <S.Container>
       <S.Wrapper>
-          <img src="../assets/logo.svg" alt="" />
+          <img src={logoImg}alt="logo" />
+          <S.Button >Nova transação</S.Button>
       </S.Wrapper>
     </S.Container>
   );
