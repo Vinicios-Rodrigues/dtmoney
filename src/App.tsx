11f144f5f533a3bf8433bda688cd0b1f19b841b7
viniciospereirarodrigues@gmail.com <viniciospereirarodrigues@gmail.com>
2021-11-21 15:38:37 -0300

    Creating Modal and functions for active

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Dashboard } from "./components/Dashboard/Dashboard";
 import Header from "./components/Header";
 import { GlobalStyle } from "./styles/GlobalStyle";
+import Modal from "react-modal";
+import { useState } from "react";
 
 function App() {
+  const [isNewtransactionsOpen, setIsNewtransactionsOpen] = useState(false);
+
+  function handleCloseNewtransaction() {
+    setIsNewtransactionsOpen(false);
+  }
+
   return (
     <div className="App">
       <Header textButton="Nova transação" />
+      <Dashboard />
+      <Modal
+        isOpen={isNewtransactionsOpen}
+        onRequestClose={handleCloseNewtransaction}
+      ></Modal>
       <GlobalStyle />
     </div>
   );
