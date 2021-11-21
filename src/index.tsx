d32599b3796194752eed424a8976d2e31cbedf3e
viniciospereirarodrigues@gmail.com <viniciospereirarodrigues@gmail.com>
2021-11-21 15:35:48 -0300

    creating server and routes with miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,23 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createServer } from "miragejs";
 
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "food",
+          createAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 ReactDOM.render(<App />, document.getElementById("root"));
